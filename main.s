7bd8cb694be83925a7732c470b3f3a2631ecfb11
Mark Beebe <mark.beebe@kroger.com>
2021-03-14 17:42:53 -0700

    Add basic Hello World program
    MPLABX deprication of MPASM means I need
    to convert everything to pic-as. Did a bit of
    this work, but still getting errors. Might
    be beyond me for now.

--- main.s
+++ main.s
@@ -1,3 +1,11 @@
-
-
-
+#include "xc.inc"
+; CONFIG
+    CONFIG WDTE = OFF, CP = OFF, MCLRE = OFF
+    ORG 0x0000H
+INIT:
+    MOVLW not(1 << GP1)
+    TRIS GPIO
+    BSF GPIO,GP1
+LOOP:
+    GOTO LOOP
+END     
